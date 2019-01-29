5f52d4e4023237aeab2353f464e12c9e5d520f29
Daniel Morgan <daniel.morgan@devopsgroup.com>
2019-01-29 14:55:52 +0000

    working kubia unstable

--- k8s/kubia/src/main.rs
+++ k8s/kubia/src/main.rs
@@ -2,7 +2,6 @@ use std::net::{TcpStream, TcpListener};
 use std::io::{Read, Write};
 use std::thread;
 
-use chrono::Utc;
 use whoami;
 
 
@@ -17,35 +16,38 @@ fn handle_read(mut stream: &TcpStream) {
     }
 }
 
-fn handle_write(mut stream: TcpStream) {
-    let request_ts = Utc::now().timestamp();
-    let mut status_code = "200";
-    if request_ts % 5 == 0 {
-        status_code = "500";
+fn handle_write(mut stream: TcpStream, counter: &u64) {
+    let mut http_status = "200 OK";
+    if counter > &4u64 {
+        http_status = "500 Internal Server Error";
     }
     let hostname = whoami::hostname();
-    let response = format!("HTTP/1.1 {} OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello {}</body></html>\r\n", status_code, hostname);
+    let response = format!("HTTP/1.1 {}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello {}</body></html>\r\n", http_status, hostname);
     let response = response.as_bytes();
     match stream.write(response) {
-        Ok(_) => println!("{} Response sent\n", status_code),
+        Ok(_) => println!("Request #{0}, '{1}' response sent\n", counter, http_status),
         Err(e) => println!("Failed sending response: {}", e),
     }
 }
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(stream: TcpStream, counter: &u64) {
     handle_read(&stream);
-    handle_write(stream);
+    handle_write(stream, &counter);
 }
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     println!("Listening for connections on port {}", 8080);
 
+    let mut counter = 0;
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(|| {
-                    handle_client(stream)
+                counter += 1;
+                println!("{}", counter);
+                thread::spawn(move || {
+                     handle_client(stream, &counter)
                 });
             }
             Err(e) => {
